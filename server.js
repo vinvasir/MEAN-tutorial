7f91bbce6b6c0b7a01d50b3e9013758a3e93c5eb
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-05-17 21:38:50 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+//
+// # SimpleServer
+//
+// A simple chat server using Socket.IO, Express, and Async.
+//
+var http = require('http');
+var path = require('path');
+
+var async = require('async');
+var socketio = require('socket.io');
+//var express = require('express');
+
+var express = require('./project/config/express');
+
+var app = express();
+app.listen(process.env.PORT, process.env.IP);
+module.exports = app;
