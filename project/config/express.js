1df16d79fd709610c5f0bd853c8b5faa2b043835
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-05-19 21:24:55 +0000

    Set up the index controller, static assets, and mongodb

--- project/config/express.js
+++ project/config/express.js
@@ -1,8 +1,11 @@
-var express = require('express'),
+var config = require('./config'),
+    express = require('express'),
     morgan = require('morgan'),
     compress = require('compression'),
     bodyParser = require('body-parser'),
-    methodOverride = require('method-override');
+    ejs = require('ejs'), // not sure if this line is necessary
+    methodOverride = require('method-override'),
+    session = require('express-session');
 
 module.exports = function(){
     var app = express();
@@ -19,6 +22,19 @@ module.exports = function(){
     app.use(bodyParser.json());
     app.use(methodOverride());
     
+    app.use(session({
+        saveUninitialized: true,
+        resave: true,
+        secret: config.sessionSecret
+    }));
+    
+    app.set('views', __dirname + '/../app/views'); //was stuck here for a while because app.set, unlike require(), 
+                                                   //needs the WHOLE file path. __dirname gets you to the current folder.
+    app.set('view engine', 'ejs');
+    
     require('../app/routes/index.server.routes.js')(app);
+    
+    app.use(express.static(__dirname + '/../public'));
+    
     return app;
 };
